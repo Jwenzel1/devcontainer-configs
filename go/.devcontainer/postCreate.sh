bfc785c3aed232522e484effaf0a7929e2acdfe9
jwenzel1 <10747934+Jwenzel1@users.noreply.github.com>
2024-10-12 14:23:43 -0400

    Fix copy and paste issues

--- go/.devcontainer/postCreate.sh
+++ go/.devcontainer/postCreate.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 set -e
-
-mkdir ~/.aws
-
-echo -e "[default]\naws_access_key_id = test\naws_secret_access_key = test" > ~/.aws/credentials
-echo -e "[default]\noutput = json" > ~/.aws/config
